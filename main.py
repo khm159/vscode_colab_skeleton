d84435c023bde39809aecb53e79fab9b594c7b8b
Hyungmin Kim <42836225+khm159@users.noreply.github.com>
2022-03-05 00:49:52 +0900

    initialization

---
+++ main.py
@@ -0,0 +1,16 @@
+
+from models import model_builder
+from opts import parser 
+
+def main_worker(opt):
+    """
+    main worker for CNN training
+    """
+    model = model_builder(opt)
+    print(model)
+    dummy = torch.zeros([32, 3, 224, 224]).cuda()
+    output = model(dummy)
+    print("output : ", output.shape)
+
+if __name__ == "__main__":
+    train(parser)
---
+++ models.py
@@ -0,0 +1,30 @@
+import torchvision.models as models 
+
+def model_builder(opt):
+    """
+    simple model builder using torchvision 
+
+    Args:
+        opt : argument parser, 실험 세팅 존재 
+
+    Raises:
+        NotImplementedError : 미구현 모델 입력시 
+        TODO: 다른 모델 추가하기 
+
+    Returns:
+        model : pytorch pretrained CNN model 
+                if selected pretrained options 
+                else default initialized model  
+    """
+    if opt.arch == 'resnet18':
+        if opt.num_classes == 1000
+            return models.resnet18(opt.pretrained).cuda()
+        else:
+            model = models.resnet18(opt.pretrained)
+            model.fc = nn.Linear(
+                models.resnet.Bottleneck.expansion*512,
+                opt.num_classes
+                )
+            return model.cuda()
+    else:
+        NotImplementedError
